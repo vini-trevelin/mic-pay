32a538c2d135bb9c6301eea9952a90009641e4f7
Vinícius Trevelin <viniciustl2000@hotmail.com>
2022-03-13 16:47:52 -0300

    teste do repositorio

---
+++ main.c
@@ -0,0 +1,51 @@
+#define F_CPU 16000000	//freq CPU (16Mhz)
+#define BAUD 9600		//baud rate
+#define UBRR F_CPU/16/BAUD-1
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+// Inicialização de USART
+
+void USART_INIT(unsigned int ubrr){
+	UBRR0H = (unsigned char)(ubrr >> 8);					//registrador taxa de transmissão parte alta
+	UBRR0L = (unsigned char)ubrr;							//registrador taxa de transmissão parte baixa
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);							//habilito transmissor e receptor
+	UCSR0C = (1<<USBS0)|(UCSZ00);							// formato do frame teste : 8 bits dados e 2 bits parada
+}
+
+// Função para enviar 8bits dados e 2 bits parada
+void USART_envia(unsigned char dados){
+	while(!(UCSR0A & (1<<UDRE0)));							// espera limpeza da transmissão (registrador)
+	UDR0 = dados;											// coloca dado a ser enviado no registrador
+}
+
+// Função para receber 8bits dados 2 bits parada
+unsigned char USART_recebe(void){
+	while(!(UCSR0A & (1<<RXC0)));							// espera algum dado ser recebido (registrador)	
+	return UDR0;											// retorna dado recebido
+}
+
+
+int main(void)
+{
+    USART_INIT(UBRR);
+    while (1) 
+    {
+		//USART_envia('S');
+		//_delay_ms(200);
+		//USART_envia('e');
+		//_delay_ms(200);
+		//USART_envia('r');
+		//_delay_ms(200);
+		//USART_envia('i');
+		//_delay_ms(200);
+		//USART_envia('a');
+		//_delay_ms(200);
+		//USART_envia('l');
+		//_delay_ms(200);
+		
+		USART_envia(USART_recebe());
+    }
+}
+
