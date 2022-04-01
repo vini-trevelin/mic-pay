5cd8d4a85e47d19786fb2658bc9a06fef18f6510
PC-VITOR <vcarvalho999@gmail.com>
2022-03-31 23:12:45 -0300

    Adicionei as funções que faltavam

--- Headers/cartoes.h
+++ Headers/cartoes.h
@@ -7,12 +7,14 @@
 #define FALSE 0
 
 char verificarContasLocais(char cartaoLido[]); 						// verificada
-char verificaCashBack (char indice,char valorVenda[]);
-char addCartao(char cardNum[],char senha[], char saldoInicial[]); 	// verificada
+char verificaCashBack (char indice,char valorVenda[]);				// verificada
+char addCartao(char cardNum[],char senha[], char saldoInicial[]); 	// REVISAR FORMA DE ENTRADA DOS DADOS
 void carregaSaldo(char saldoInic[]);								// verificada
 void carregaNumCartao(char numCard[]); 								// verificada
 void carregaSenha(char senha[]);  									// verificada
-char executarVendaInterna(char indice,char valor[], char senha[]); 	
-char subtraiSaldo(char indice,char venda[]);
-char verificaSaldo (char indice, char valorVenda[],char temCashBack);
-char verificaSenha(char indice, char senhaRecebida[]);
+char executarVendaInterna(char indice,char valor[], char senha[]);  // verificada	
+void subtraiSaldo(char indice,char venda[]);					    // verificada
+char verificaSaldo (char indice, char valorVenda[]);				// verificada
+char verificaSenha(char indice, char senhaRecebida[]);				// verificada
+void atualizaSenha(char indice,char novaSenha[]); 					// REVISAR FORMA DE ENTRADA DOS DADOS
+void adicionaSaldo(char indice,char valorAdd[]);					// REVISAR FORMA DE ENTRADA DOS DADOS
--- Modulos/cartoes.c
+++ Modulos/cartoes.c
@@ -5,11 +5,15 @@
  *  char verificaContasLocais (char cartaoLido[])  retorna a (posicaoDoCartao+ 1) e zero se não Existir
  *  char addCartao(char cardNum[],char senha[], char saldoInicial[]) # cria uma nova conta se possivel (true/1) ou false(0);
  *  char executarVendaInterna(char indice,char valor[], char senha[]) # executa uma venda se possivel (true/1)  ou retorna erro (false/0)
- *  char atualizaSenha(char novaSenha[])  #atualiza a senha // falta implementar
- *  char adicionaSaldo(char valor[],char senha[]) retorna true se possivel adicionar (saldo total menor que 999,99) //falta implementar
+ *  void atualizaSenha(char indice,char novaSenha[])  #atualiza a senha 
+ *  char adicionaSaldo(char valor[],char senha[]) retorna true se possivel adicionar (saldo total menor que 999,99) 
  *  char verificaCashBack(char indice,char valorVenda[])  retorna true se cashBack será aplicado 
+ *  TAREFAS: Otimizar adiciona saldo e subtrai Saldo, verificar forma de entrada da nova senha e forma de entrada do valor a ser adicionado
+ *  e cartão e etc.
  */
 //==================================================================
+#include "../Headers/cartoes.h"
+#include "../Headers/lcd.h"
 
 
 //static para manterem os valores durante as incializações
@@ -17,12 +21,72 @@ static char numCartoesLocais [LOCALCARDNUM][CARDSIZE]; // indices correspondente
 static char senhasLocais[LOCALCARDNUM][SENHASIZE];
 static char cashBackCounter[LOCALCARDNUM];
 static char saldosLocais[LOCALCARDNUM][DIGITOS]; // armazenados na forma mais significativo -> indice 0;
-static char cartoesCadastrados =0; // inicia sem nenhum cadastro;
+unsigned static char cartoesCadastrados; // inicia sem nenhum cadastro;
 
+void atualizaSenha(char indice,char novaSenha[]){ // recebe o indice obtido por verificaContasLocais
+	unsigned char i=0;
+	for (i = 0;i<SENHASIZE;i++)
+	{
+		senhasLocais[indice-1][i]= novaSenha[i];
+	}
+}
 
+void adicionaSaldo(char indice,char valorAdd[]){ // verificar como receberei o vetor do saldo a adicionar (se será igual a um valor de venda ou não ??)
+	//fiz como se não fosse
+	unsigned char k = indice -1,i;
+	int centavos=0;
+	int inteiros=0;
+	int mul=0;
+	// eventualmente criar uma função que converte de char  array para inteiros
+	for (i =DIGITOS -1 ; i>=0; i--){
+		switch(i){
+			case 0:
+			mul=1;
+			break;
+			case 1:
+			mul=10;
+			break;
+			case 2:
+			mul=1;
+			break;
+			case 3:
+			mul=10;
+			break;
+			case 4:
+			mul=100;
+			break;
+			default: mul=0;
+			break;
+		}
+		if(i<2){
+			centavos +=(int) (saldosLocais[k][i]+valorAdd[i]-96)*mul; //-96 para eliminar o efeito da ASCII
+
+		}
+		else {
+			inteiros +=(int) (saldosLocais[k][i]+valorAdd[i]-96)*mul; //-96 para eliminar o efeito da ASCII
+		}
+	}
+	if (centavos>= 100){
+		inteiros = inteiros+1;
+		centavos =centavos -100;
+	}
+	if(inteiro>=1000){
+		return FALSE;
+	}
+	else{
+	saldosLocais[k][4] = (char)(centavos%10) +48; //centesimos
+	saldosLocais[k][3] = (char)(centavos/10) +48;  //decimos
+	saldosLocais[k][0] = (char)(inteiros/100);
+	inteiros = inteiros- saldosLocais[k][0]*100; 
+	saldosLocais[k][0] =  saldosLocais[k][0]+48; //centenas
+	saldosLocais[k][1] = (char)(inteiros/10)+48; //dezenas
+	saldosLocais[k][2]	= (char)(inteiros%10)+48; // unidades
+	}
+}
+	
 
 char verificarContasLocais(char cartaoLido[]){ // verificada
-	char i,j,flag= FALSE;
+	unsigned char i,j;
 	for (i=0;i<LOCALCARDNUM;i++){
 		for (j=0;j<CARDSIZE;j++){
 			if(numCartoesLocais[i][j]!=cartaoLido[j]){
@@ -60,12 +124,13 @@ char addCartao(char cardNum[],char senha[], char saldoInicial[]){ // retorna TRU
 		cartoesCadastrados++; // cadastrei mais um cartão
 		return TRUE;
 	}
-	return FALSE;
+		return FALSE;
+	
 }
 
 //Verificada
 void carregaSaldo(char saldoInic[]){ // saldo inic deve estar em ASCII
-	char i=0;
+	unsigned char i=0;
 	for (i = 0;i<DIGITOS;i++)
 	{
 		saldosLocais[cartoesCadastrados][i]= saldoInic[i];
@@ -74,7 +139,7 @@ void carregaSaldo(char saldoInic[]){ // saldo inic deve estar em ASCII
 
 //Verificada
 void carregaNumCartao(char numCard[]){
-	unsigned char i=0;
+	unsigned  char i=0;
 	for (i = 0;i<CARDSIZE;i++)
 	{
 		numCartoesLocais[cartoesCadastrados][i]= numCard[i];
@@ -83,7 +148,7 @@ void carregaNumCartao(char numCard[]){
 
 //Verificada
 void carregaSenha(char senha[]){ //senha deve estar em ASCII
-	char i=0;
+	unsigned char i=0;
 	for (i = 0;i<SENHASIZE;i++)
 	{
 		senhasLocais[cartoesCadastrados][i]= senha[i];
@@ -100,7 +165,7 @@ void carregaSenha(char senha[]){ //senha deve estar em ASCII
 //									retorna true
 char executarVendaInterna(char indice,char valor[], char senha[]){ // acho que como é algo interno não é interessante mandar uma mesagem, só dizer se é possivel
 								// Retorno TRUE ou se e  falso// retorno FALSE 
-	char auxiliar = verificaSenha(indice,senha);
+	unsigned char auxiliar = verificaSenha(indice,senha);
 	if (auxiliar){ //se senha valida
 		auxiliar = verificaCashBack(indice,valor); // dessa forma é possivel comprar algo de 50 pila com 30 pila de saldo se  estou fazendo minha 3 compra, valoriza o cliente local
 		if(auxiliar) {
@@ -110,7 +175,7 @@ char executarVendaInterna(char indice,char valor[], char senha[]){ // acho que c
 		else{
 			cashBackCounter[indice-1]=cashBackCounter[indice-1] +1; // somente para deixar bem explicito
 		}
-		auxiliar = verificaSaldo(indice,valor,auxiliar);
+		auxiliar = verificaSaldo(indice,valor);
 		if (auxiliar) {
 			subtraiSaldo(indice,valor);
 			return TRUE;
@@ -126,14 +191,13 @@ char executarVendaInterna(char indice,char valor[], char senha[]){ // acho que c
 	
 }
 
-char subtraiSaldo(char indice,char venda[]){
+void subtraiSaldo(char indice,char venda[]){
 	//como já verifique é chars são numeros em ASCII -> posso só subtrailos e vou eliminar o offset
 	// depois tenho que somar o  48 é 0 em ASCII
-	char k = indice -1,i;
+	unsigned char k = indice -1,i;
 	int centavos=0;
 	int inteiros=0;
 	int mul=0;
-	int aux= 0;
 	for (i =0 ; i< DIGITOS; i++){
 		switch(i){
 			case 0:
@@ -155,8 +219,6 @@ char subtraiSaldo(char indice,char venda[]){
 				break;
 		}
 		if(i<2){
-			aux = (saldosLocais[k][DIGITOS-1-i]);
-			aux -= venda[i];
 			centavos +=(int) (saldosLocais[k][DIGITOS-1-i]-venda[i])*mul;
 
 		}
@@ -179,8 +241,8 @@ char subtraiSaldo(char indice,char venda[]){
 
 }
 
-char verificaSaldo (char indice, char valorVenda[],char temCashBack){ // Retorna TRUE ou FALSE de acordo com a disponibilidade de saldo
-	char k = indice -1; // somente para as expressões ficarem mais claras, de certa forma é um disperdício de performace 
+char verificaSaldo (char indice, char valorVenda[]){ // Retorna TRUE ou FALSE de acordo com a disponibilidade de saldo
+	unsigned char k = indice -1; // somente para as expressões ficarem mais claras, de certa forma é um disperdício de performace 
 	// venda tem 5 caracteres e eles estão na ordem invertida. Ex: R$ 652,43 -> {3, 4 ,2, 5, 6}
 	// Depois organizar isso para fazer com um for, está muito hardcoded
 	if      (saldosLocais[k][0] >  valorVenda[4]) {return TRUE;}  //centenas
@@ -198,8 +260,8 @@ char verificaSaldo (char indice, char valorVenda[],char temCashBack){ // Retorna
 	else return FALSE;
 }
 	
-char verificaSenha(char indice, char senhaRecebida[]){
-	char i;
+ char verificaSenha(char indice, char senhaRecebida[]){
+	unsigned char i;
 	for (i =0;i<SENHASIZE;i++)
 	{
 		if(senhaRecebida[i] != senhasLocais[indice-1][i]){
@@ -209,5 +271,5 @@ char verificaSenha(char indice, char senhaRecebida[]){
 			return TRUE;
 		}
 	}
+	return TRUE;
 }
-
